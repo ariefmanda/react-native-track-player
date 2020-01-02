d23c0325fc529de74cd5c46d659ab5e3eb0c45e5
ariefAleph <arief.trimanda@aleph-labs.com>
2020-01-02 12:20:02 +0700

    [iOS] Fix : clean code playback-track-changed on swift

--- ios/RNTrackPlayer/RNTrackPlayer.swift
+++ ios/RNTrackPlayer/RNTrackPlayer.swift
@@ -185,17 +185,20 @@ public class RNTrackPlayer: RCTEventEmitter {
         player.event.playbackEnd.addListener(self) { [weak self] reason in
             guard let `self` = self else { return }
 
-            if reason == .playedUntilEnd && self.player.nextItems.count == 0 {
-                self.sendEvent(withName: "playback-queue-ended", body: [
-                    "track": (self.player.currentItem as? Track)?.id,
-                    "position": self.player.currentTime,
-                    ])
-            } else if reason == .playedUntilEnd {
+            if reason == .playedUntilEnd {
+                
                self.sendEvent(withName: "playback-track-changed", body: [
                     "track": (self.player.currentItem as? Track)?.id,
                     "position": self.player.currentTime,
                     "nextTrack": (self.player.nextItems.first as? Track)?.id,
+                ])
+                
+                if self.player.nextItems.count == 0 {
+                    self.sendEvent(withName: "playback-queue-ended", body: [
+                        "track": (self.player.currentItem as? Track)?.id,
+                        "position": self.player.currentTime,
                     ])
+                }
             }
         }
         
